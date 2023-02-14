231b1b95ca6e6c0df05439b6793875fa3c486cb3
sixdgt <c4crypt@gmail.com>
2023-02-14 17:02:47 +0545

    urls, template & views demo with config

--- project_employee_record_system/app_employeewebsite/views.py
+++ project_employee_record_system/app_employeewebsite/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def demo(request):
+    context = {
+        "class_name": "Django Class",
+        "date": "2023-02-14",
+        "day": "Tuesday"
+    }
+    return render(request, 'demo.html', context)
--- project_employee_record_system/project_employee_record_system/urls.py
+++ project_employee_record_system/project_employee_record_system/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('employee/', include('app_employeewebsite.urls')),
+    path('', include('app_employeewebsite.urls')), # incase if need to load via base url
+    
 ]
