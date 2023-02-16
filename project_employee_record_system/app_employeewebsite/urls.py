7b836c26fc9d03aac1fc31ef6d60d726177f4f8d
sixdgt <c4crypt@gmail.com>
2023-02-16 17:12:11 +0545

    master page & employee add-view-edit-show page created

--- project_employee_record_system/app_employeewebsite/urls.py
+++ project_employee_record_system/app_employeewebsite/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('demo/', views.demo, name='demo'),
-    path('', views.demo, name='demo'),
+    path('employee/', views.employee_index, name='emp-index'),
+    path('employee/show/', views.employee_show, name='emp-show'),
+    path('employee/edit/', views.employee_edit, name='emp-edit'),
+    path('employee/add/', views.employee_add, name='emp-add'),
 ]
--- project_employee_record_system/app_employeewebsite/views.py
+++ project_employee_record_system/app_employeewebsite/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
-def demo(request):
-    context = {
-        "class_name": "Django Class",
-        "date": "2023-02-14",
-        "day": "Tuesday"
-    }
-    return render(request, 'demo.html', context)
+def employee_index(request):
+    return render(request, 'employees/index_employee.html')
+
+def employee_add(request):
+    return render(request, 'employees/add_employee.html')
+
+def employee_edit(request):
+    return render(request, 'employees/edit_employee.html')
+
+def employee_show(request):
+    return render(request, 'employees/show_employee.html')
