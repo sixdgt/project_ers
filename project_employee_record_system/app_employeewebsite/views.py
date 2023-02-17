4b353be7145520d91d269f897f95287366784fdd
sixdgt <c4crypt@gmail.com>
2023-02-17 17:10:08 +0545

    employee add form class-templated created

--- project_employee_record_system/app_employeewebsite/views.py
+++ project_employee_record_system/app_employeewebsite/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from .forms import EmployeeCreateForm
 
 # Create your views here.
 def employee_index(request):
     return render(request, 'employees/index_employee.html')
 
 def employee_add(request):
-    return render(request, 'employees/add_employee.html')
+    emp_create_form = EmployeeCreateForm()
+    context = {"form": emp_create_form}
+    return render(request, 'employees/add_employee.html', context)
 
 def employee_edit(request):
     return render(request, 'employees/edit_employee.html')
