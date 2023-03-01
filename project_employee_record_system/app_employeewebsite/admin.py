6409e40bd6f63eb2a77ca401647c0c802ed606a0
sixdgt <c4crypt@gmail.com>
2023-03-01 17:09:27 +0545

    admin panel, static files and bootstrap theme

--- project_employee_record_system/app_employeewebsite/admin.py
+++ project_employee_record_system/app_employeewebsite/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from .models import Department, Employee
 
 # Register your models here.
+class AdminEmployee(admin.ModelAdmin):
+    list_display = ("full_name", "join_date", "email", "contact")
+    search_fields = ("full_name", "email")
+    list_filter = ("full_name", "email")
+
 admin.site.register(Department)
-admin.site.register(Employee)
+admin.site.register(Employee, AdminEmployee)
+
+admin.site.site_header = 'ERS'
+admin.site.site_title = 'ERS'
+admin.site.index_title = 'Admin Panel'
